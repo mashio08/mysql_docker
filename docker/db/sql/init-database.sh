14d2f1d47365fb992ac63fd8d2aa6684b00906e0
杉満寿央 <sugi@sugimanjuuounoMacBook-Pro.local>
2020-11-07 05:17:03 +0900

    不要ファイル除去

--- docker/db/sql/init-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#wait for the MySQL Server to come up
-#sleep 90s
-
-#run the setup script to create the DB and the schema in the DB
-mysql -u root -proot -h 127.0.0.1 < "/docker-entrypoint-initdb.d/init_ddl.sql"
